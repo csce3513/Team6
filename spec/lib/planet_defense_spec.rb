92bce020407adc6b7d2bbe90eb8490eee861fb4c
Addam Hardy <addam.hardy@gmail.com>
2012-02-04 11:12:27 -0600

    modularizing the spec

--- spec/lib/planet_defense_spec.rb
+++ spec/lib/planet_defense_spec.rb
@@ -4,29 +4,31 @@ require_relative '../../lib/planet_defense/player'
 require_relative '../../lib/planet_defense/play_state'
 require_relative '../../lib/planet_defense/main'
 
-describe PlanetDefense do
+module PlanetDefense
+  describe GameWindow do
   
-  before(:all) do
-    @g = PlanetDefense::GameWindow.new
-  end
+    before(:all) do
+      @g = PlanetDefense::GameWindow.new
+    end
 
-  it { @g.should respond_to :close }
-  it { @g.should respond_to :fps }
-  it { @g.should respond_to :update }
-  it { @g.should respond_to :draw }
-  it { @g.should respond_to :root }
-  it { @g.should respond_to :game_state_manager }
-  it { @g.should respond_to :factor }
-  it { @g.should respond_to :cursor }
-  it { @g.should respond_to :root }
-  it { @g.should respond_to :milliseconds_since_last_tick }
+    it { @g.should respond_to :close }
+    it { @g.should respond_to :fps }
+    it { @g.should respond_to :update }
+    it { @g.should respond_to :draw }
+    it { @g.should respond_to :root }
+    it { @g.should respond_to :game_state_manager }
+    it { @g.should respond_to :factor }
+    it { @g.should respond_to :cursor }
+    it { @g.should respond_to :root }
+    it { @g.should respond_to :milliseconds_since_last_tick }
   
-  it 'Window should be initialized' do
-    @g.should be_kind_of(PlanetDefense::GameWindow)
-  end
+    it 'Window should be initialized' do
+      @g.should be_kind_of(PlanetDefense::GameWindow)
+    end
   
-  it 'Window should have correct dimensions' do
-    @g.width.should == 1024
-    @g.height.should == 768
+    it 'Window should have correct dimensions' do
+      @g.width.should == 1024
+      @g.height.should == 768
+    end
   end
 end
