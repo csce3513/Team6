ee54ffbeab6d9d22587dfeb24f4119a687d53e10
Addam Hardy <addam.hardy@gmail.com>
2012-02-21 14:16:35 -0600

    Temporarily changed difficulty

--- lib/planet_defense/play_state.rb
+++ lib/planet_defense/play_state.rb
@@ -6,7 +6,7 @@ module PlanetDefense
     def initialize( options = {})
       super
       @player = Player.new(self)  
-      @@asteroids = 5.times.map { Asteroid.new(self) }
+      @@asteroids = 20.times.map { Asteroid.new(self) }
       @background_image = Gosu::Image.new($window, "media/gfx/space-with-earth.jpg", true)
       @life_image = Gosu::Image.new($window, "media/gfx/shipSmall.png", true)
       @music = Gosu::Song.new($window, "media/sounds/background.wav")
