255da7bb1cb847a64e10ecc4d688d5876c5c6864
Addam Hardy <addam.hardy@gmail.com>
2012-02-23 09:44:07 -0600

    WIP on shooting lasers spec

--- spec/lib/player_spec.rb
+++ spec/lib/player_spec.rb
@@ -24,21 +24,7 @@ module PlanetDefense
     context "when playing" do
       before :each do
         @g.update
-    end
-
-
-    #--------
-    #MOVEMENT
-    #--------
-
-    it 'should move left across the screen on command' do
-      @player.x = $window.width / 2
-      20.times do
-        @player.move_left
-        @player.move
       end
-      @player.x.should <= 512
-    end
 
 
       #--------
@@ -53,16 +39,28 @@ module PlanetDefense
         end
         @player.x.should <= 512
       end
-      @player.x.should >= 512
-    end
 
-    it 'should move up the screen on command' do
-      20.times do
-        @player.move_forward
-        @player.move
+
+      #--------
+      #MOVEMENT
+      #--------
+
+      it 'should move left across the screen on command' do
+        @player.x = $window.width / 2
+        20.times do
+          @player.move_left
+          @player.move
+        end
+        @player.x.should <= 512
       end
-      @player.y.should <= 718
-    end
+
+      it 'should move up the screen on command' do
+        20.times do
+          @player.move_forward
+          @player.move
+        end
+        @player.y.should <= 718
+      end
 
     it 'should move down the screen on command' do
       @player.y = 718
@@ -92,15 +90,13 @@ module PlanetDefense
         end
         @player.x.should > 0
       end
-      @player.x.should > 0
-    end
 
-    it 'should not exceed the right boundary' do
-      1000.times do
-        @player.move_right
+      it 'should not exceed the right boundary' do
+        1000.times do
+          @player.move_right
+        end
+        @player.x.should < 1024
       end
-      @player.x.should < 1024
-    end
 
       it 'should not exceed the top boundary' do
         1000.times do
@@ -108,8 +104,6 @@ module PlanetDefense
         end
         @player.y.should > 0
       end
-      @player.x.should > 0
-    end
 
       it 'should not exceed the bottom boundary' do
         1000.times do
@@ -117,8 +111,6 @@ module PlanetDefense
         end
         @player.y.should < 768
       end
-      @player.x.should < 768
-    end
 
 
       #--------------
@@ -159,39 +151,40 @@ module PlanetDefense
     
     end
 
-    it 'should heat up when shot to 100 maximum' do
-      @player.laser_heat = 0
-      10.times do
-        @lastHeat = @player.laser_heat
-        @player.lastShot = 0;
-        @player.heat_up_laser
-        @player.laser_heat.should > @lastHeat
+      it 'should heat up when shot to 100 maximum' do
+        @player.laser_heat = 0
+        10.times do
+          @lastHeat = @player.laser_heat
+          @player.lastShot = 0;
+          @player.heat_up_laser
+          @player.laser_heat.should > @lastHeat
+        end
+        @player.laser_heat.should <= 100
       end
-      @player.laser_heat.should <= 100
-    end
 
-    it 'should overheat at 100 heat, and give a firing penalty' do
-      @player.laser_heat = 100
-      @player.overheated?.should == true
-      @player.lastShot.should > milliseconds()
-    end
-
-    it 'should cooldown every @cooldown_time' do
+      it 'should overheat at 100 heat, and give a firing penalty' do
         @player.laser_heat = 100
-        if (@player.last_cooldown + @player.cooldown_time) < milliseconds()
-          @player.move
-          @player.laser_heat.should < 100
-        end
-    end
-
-    it 'should have a restricted firing rate' do
-      @no_shot = 0
-      10.times do
-        if (!@player.shoot)
-          @no_shot += 1
-        end
+        @player.overheated?.should == true
+        @player.lastShot.should > milliseconds()
       end
-        @no_shot.should > 0
+
+      it 'should cooldown every @cooldown_time' do
+          @player.laser_heat = 100
+          if (@player.last_cooldown + @player.cooldown_time) < milliseconds()
+            @player.move
+            @player.laser_heat.should < 100
+          end
+      end
+
+      it 'should have a restricted firing rate' do
+        @no_shot = 0
+        10.times do
+          if (!@player.shoot)
+            @no_shot += 1
+          end
+        end
+          @no_shot.should > 0
+      end
     end
 
 
