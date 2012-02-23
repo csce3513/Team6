775a90fa57b9b9cba704c5aa3decf162c9f9de15
Michael Gammon <mgammon@uark.edu>
2012-02-22 20:56:05 -0600

    Added laser heatup/cooldown, plus a penalty if fully heated

--- lib/planet_defense/play_state.rb
+++ lib/planet_defense/play_state.rb
@@ -6,7 +6,7 @@ module PlanetDefense
     def initialize( options = {})
       super
       @player = Player.new(self)  
-      @@asteroids = 20.times.map { Asteroid.new(self) }
+      @@asteroids = 5.times.map { Asteroid.new(self) }
       @background_image = Gosu::Image.new($window, "media/gfx/space-with-earth.jpg", true)
       @music = Gosu::Song.new($window, "media/sounds/background.wav")
       @font = Gosu::Font.new($window, "media/fonts/MuseoSans_300.otf", 43)
