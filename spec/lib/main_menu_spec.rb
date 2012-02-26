dacd0dcca2e1c35ab404b1472a8fc82552ce9cb8
Addam Hardy <addam.hardy@gmail.com>
2012-02-25 22:15:50 -0600

    work on specs for navigating menus

--- spec/lib/main_menu_spec.rb
+++ spec/lib/main_menu_spec.rb
@@ -5,7 +5,7 @@ module PlanetDefense
   
     before :each do
       @g = PlanetDefense::GameWindow.new
-      @currentState = $window.current_game_state
+      @cs = $window.current_game_state
     end
     
     after :each do
@@ -13,23 +13,28 @@ module PlanetDefense
     end
     
     it 'should be correct gamestate' do
-      @currentState.should be_kind_of(PlanetDefense::MenuState)
+      @cs.should be_kind_of(PlanetDefense::MenuState)
     end
 
     it 'should go to options screen' do
-      @currentState.move_down
-      @currentState.go
+      @cs.move_down
+      @cs.go
       $window.current_game_state.should be_kind_of(PlanetDefense::OptionsState)
     end
 
    it 'should return to main menu when "Back" is selected' do
       @g.push_game_state(OptionsState)
-      @currentState.move_down
-      @currentState.move_down
-      @currentState.go
+      @cs.move_down
+      @cs.move_down
+      @cs.go
       $window.current_game_state.should be_kind_of(PlanetDefense::MenuState)
     end
 
+    it 'should go to other menu options' do
+      @cs.move_down
+      @cs.go
+      $window.current_game_state.should be_kind_of(PlanetDefense::OptionsState)
+    end
 
   end
 end
