cc1c941b7b77914ec0e3c19dd3656bc6c9418b97
Denton Alford <alford235@gmail.com>
2012-02-27 15:18:05 -0600

    Redoing changes accidentally lost

--- spec/lib/main_menu_spec.rb
+++ spec/lib/main_menu_spec.rb
@@ -3,25 +3,33 @@ require 'spec_helper'
 module PlanetDefense
   describe MenuState do
   
-    before :all do
+    before :each do
       @g = PlanetDefense::GameWindow.new
+      @currentState = $window.current_game_state
     end
     
-    after :all do
+    after :each do
       @g.close
     end
     
     it 'should be correct gamestate' do
+      @currentState.should be_kind_of(PlanetDefense::MenuState)
+    end
+
+    it 'should go to options screen' do
+      @currentState.move_down
+      @currentState.go
+      $window.current_game_state.should be_kind_of(PlanetDefense::OptionsState)
+    end
+
+   it 'should return to main menu when "Back" is selected' do
+      @g.push_game_state(OptionsState)
+      @currentState.move_down
+      @currentState.move_down
+      @currentState.go
       $window.current_game_state.should be_kind_of(PlanetDefense::MenuState)
     end
 
-    it 'should return to main menu when "Back" is selected' do
-      @g.push_game_state(OptionsState)
-      @cs.move_down
-      @cs.move_down
-      @cs.go
-      $window.current_game_state.should be_kind_of(PlanetDefense::MenuState)
-    end
 
   end
 end
