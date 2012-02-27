f4fb13a2a18dad0bcfd628e1b7fc24a95189e09c
Denton Alford <alford235@gmail.com>
2012-02-27 15:11:44 -0600

    Testing back button on options screen

--- spec/lib/main_menu_spec.rb
+++ spec/lib/main_menu_spec.rb
@@ -15,5 +15,13 @@ module PlanetDefense
       $window.current_game_state.should be_kind_of(PlanetDefense::MenuState)
     end
 
+    it 'should return to main menu when "Back" is selected' do
+      @g.push_game_state(OptionsState)
+      @cs.move_down
+      @cs.move_down
+      @cs.go
+      $window.current_game_state.should be_kind_of(PlanetDefense::MenuState)
+    end
+
   end
 end
