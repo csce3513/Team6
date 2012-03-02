72fc4c7e0dacc82243013d71ff71bdc3e9c1281e
Michael Gammon <mgammon@uark.edu>
2012-03-01 23:02:31 -0600

    Mostly finished weapon class/test (hopefully)

--- lib/planet_defense/weapon.rb
+++ lib/planet_defense/weapon.rb
@@ -2,7 +2,7 @@ module PlanetDefense
       attr_reader :weapon
       class Weapon
 
-		def initialize()
+		def initialize(options = {})
 			#How often (in ms) the weapon cools down by 1 (Rate of cooldown)
 			@cooldown_rate = 100
 			#Amount of heat generated each shot
@@ -12,7 +12,7 @@ module PlanetDefense
 			#Length of time weapon is disabled if overheated
 			@overheat_penalty = 1000
 			#Time of last shot
-			@last_shot = milliseconds()
+			@last_shot = 0
                   #Time of last cooldown
                   @last_cooldown = milliseconds()
                   #Time of last overheat
@@ -23,7 +23,7 @@ module PlanetDefense
 			@overheated = false
                   @gauge_color = Gosu::Color::GREEN
 		end
-            attr_accessor :cooldown_rate, :heatup_amount, :fire_rate, :overheat_penalty, :last_shot, :last_cooldown, :heat, :overheated, :gauge_color 
+            attr_accessor :cooldown_rate, :heatup_amount, :fire_rate, :overheat_penalty, :last_shot, :last_cooldown, :last_overheat, :heat, :overheated, :gauge_color 
       
             def reset
                   initialize()
@@ -56,15 +56,17 @@ module PlanetDefense
 
             def gauge_color?
                   if !@overheated
-                        if @heat >= 0 && @heat <= 33
+                        if @heat >= 0 && @heat <= 25
                               @gauge_color = Gosu::Color::GREEN
-                        elsif @heat > 33 && @heat < 66
-                              @gauge_color = Gosu::Color::ORANGE
-                        else @heat > 66
+                        elsif @heat > 25 && @heat < 50
+                              @gauge_color = Gosu::Color::YELLOW
+                        elsif @heat > 50 && @heat < 75
+                              @gauge_color = Gosu::Color.argb(0xffffa500)
+                        else @heat >= 75
                               @gauge_color = Gosu::Color::RED
                         end
                   else
-                        @gauge_color = Gosu::Color::GREY
+                        @gauge_color = Gosu::Color::GRAY
                   end
             end
 
@@ -74,7 +76,7 @@ module PlanetDefense
                         @overheated = true
                   else
                         #If overheat penalty time is over, not overheated anymore
-                        if @last_overheat + @overheat_penalty =< milliseconds()
+                        if @last_overheat + @overheat_penalty <= milliseconds()
                               @overheated = false
                         else
                               @overheated = true
@@ -82,9 +84,26 @@ module PlanetDefense
                   end
             end
 
+            def fireable?
+                  if (!@overheated && @last_shot + @fire_rate < milliseconds())
+                        true
+                  else
+                        false
+                  end
+            end
+
+
             def update
+                  cooldown?
                   overheated?
                   gauge_color?
+            end
+
+            def shoot
+                  if fireable?
+                        PlanetDefense::Laser.create( :x => @player.x-20, :y => @player.y-15)
+                        PlanetDefense::Laser.create( :x => @player.x+20, :y => @player.y-15)
+                  end
             end
 
 	end
--- spec/lib/weapon_spec.rb
+++ spec/lib/weapon_spec.rb
@@ -14,8 +14,9 @@ module PlanetDefense
         @weapon.heatup_amount.should_not == nil
         @weapon.fire_rate.should >= 0 
         @weapon.overheat_penalty.should_not == nil 
-        @weapon.last_shot.should_not == nil 
+        @weapon.last_shot.should == 0
         @weapon.last_cooldown.should_not == nil 
+        @weapon.last_overheat.should == 0
         @weapon.heat.should == 0
         @weapon.overheated.should == false 
         @weapon.gauge_color.should == Gosu::Color::GREEN
@@ -41,24 +42,40 @@ module PlanetDefense
         5.times do
           @weapon.heatup
         end
-        @weapon.heat.should == @weapon.heatup_amount * 5
+        @weapon.heat.should == (@weapon.heatup_amount * 5)
+
+        @weapon.heat = 100
+        @weapon.heatup
+        @weapon.heat.should <= 100
       end
 
       it 'should change the gauge color according to heat' do
-        @weapon.heat += 33
+        @weapon.heat += 20
         @weapon.gauge_color?.should == Gosu::Color::GREEN
+
+        @weapon.heat += 20
+        @weapon.gauge_color?.should == Gosu::Color::YELLOW
         
-        @weapon.heat += 33
-        @weapon.gauge_color?.should == Gosu::Color::ORANGE
+        @weapon.heat += 20
+        @weapon.gauge_color?.should == Gosu::Color.argb(0xffffa500)
         
-        @weapon.heat += 33
+        @weapon.heat += 20
         @weapon.gauge_color?.should == Gosu::Color::RED
 
-        @weapon.heat = 100
+        @weapon.heat = 150
         @weapon.overheated?
-        @weapon.gauge_color?.should == Gosu::Color::GREY
+        @weapon.gauge_color?.should == Gosu::Color::GRAY
       end
 
+      it 'should have a restricted firing rate' do
+        @weapon.fireable?.should == true
+
+        @weapon.last_shot = milliseconds() + 1000
+        @weapon.fireable?.should == false
+
+        @weapon.overheated = true
+        @weapon.fireable?.should == false
+      end
     end
   end
 end
