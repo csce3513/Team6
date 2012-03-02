8ab608ce28856a59dbe068d7eef7c8fc64238936
Bryan Glazer <bglazer@uark.edu>
2012-03-01 22:37:22 -0600

    Debugging explosion

--- lib/planet_defense/explosion.rb
+++ lib/planet_defense/explosion.rb
@@ -2,24 +2,24 @@ module PlanetDefense
 	class Explosion < Chingu::GameObject
 		
 		def initialize(options)
+			super
 			@x = options[:x]
 			@y = options[:y]
 			
-			@anim_options = { :loop => false, :file => "media/gfx/explosion_strip.png", :width => 50, :height => 100 }
-			@explosion_anim = Chingu::Animation.new( @anim_options )
-			@explosion_anim.next!
-			@image = @explosion_anim.image	
-			puts("initialize");
+			@anim_options = { :loop => true, :file => "media/gfx/explosion_strip.png", :width => 50, :height => 100, :delay => 10}
+			@anim = Chingu::Animation.new( @anim_options )
+			puts( @anim )
+			@image = @anim.first
+			puts( @anim.index )
 		end
 		
 		def update
-			@explosion_anim.next!
-			@image = @explosion_anim.image
-			puts("update")
+			@image = @anim.next
+			
 		end
 		
 		def draw
-			puts("draw")
+			@image.draw(@x, @y)
 		end
 	end
 end
