42ca5b470c98c507f3e3874763e01088bcfd0043
Michael Gammon <mgammon@uark.edu>
2012-03-01 22:26:47 -0600

    More work on the weapon class/test

--- lib/planet_defense/weapon.rb
+++ lib/planet_defense/weapon.rb
@@ -15,6 +15,8 @@ module PlanetDefense
 			@last_shot = milliseconds()
                   #Time of last cooldown
                   @last_cooldown = milliseconds()
+                  #Time of last overheat
+                  @last_overheat = 0
 			#Current heat level
 			@heat = 0
 			#Boolean for if weapon is overheated
@@ -43,6 +45,46 @@ module PlanetDefense
                         puts "test"
                         return false
                   end
+            end
+
+            def heatup
+                  @heat += @heatup_amount
+                  if @heat > 100
+                        @heat = 100
+                  end
+            end
+
+            def gauge_color?
+                  if !@overheated
+                        if @heat >= 0 && @heat <= 33
+                              @gauge_color = Gosu::Color::GREEN
+                        elsif @heat > 33 && @heat < 66
+                              @gauge_color = Gosu::Color::ORANGE
+                        else @heat > 66
+                              @gauge_color = Gosu::Color::RED
+                        end
+                  else
+                        @gauge_color = Gosu::Color::GREY
+                  end
+            end
+
+            def overheated?
+                  #If the heat is over 100, it is overheated
+                  if (@heat >= 100)
+                        @overheated = true
+                  else
+                        #If overheat penalty time is over, not overheated anymore
+                        if @last_overheat + @overheat_penalty =< milliseconds()
+                              @overheated = false
+                        else
+                              @overheated = true
+                        end
+                  end
+            end
+
+            def update
+                  overheated?
+                  gauge_color?
             end
 
 	end
--- spec/lib/weapon_spec.rb
+++ spec/lib/weapon_spec.rb
@@ -29,14 +29,36 @@ module PlanetDefense
       end
 
       it 'should cooldown according to cooldown_rate' do
+        #Set heat to 100, then wait 1 second so it cools down
         @weapon.heat = 100
         sleep(1)
         if @weapon.cooldown?
           @weapon.heat.should == (100 - (milliseconds() - @weapon.last_cooldown) / @weapon.cooldown_rate) || 0
         end
       end
+
+      it 'should heat up according to heatup_amount' do
+        5.times do
+          @weapon.heatup
+        end
+        @weapon.heat.should == @weapon.heatup_amount * 5
+      end
+
+      it 'should change the gauge color according to heat' do
+        @weapon.heat += 33
+        @weapon.gauge_color?.should == Gosu::Color::GREEN
+        
+        @weapon.heat += 33
+        @weapon.gauge_color?.should == Gosu::Color::ORANGE
+        
+        @weapon.heat += 33
+        @weapon.gauge_color?.should == Gosu::Color::RED
+
+        @weapon.heat = 100
+        @weapon.overheated?
+        @weapon.gauge_color?.should == Gosu::Color::GREY
+      end
+
     end
-
-
   end
 end
