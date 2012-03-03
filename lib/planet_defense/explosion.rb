efaf50572e152eb784f48add5bbd92ffcb017b41
Bryan Glazer <bglazer@uark.edu>
2012-03-03 17:35:34 -0600

    Removed frags from asteroid init

--- lib/planet_defense/explosion.rb
+++ lib/planet_defense/explosion.rb
@@ -5,10 +5,9 @@ module PlanetDefense
       super
       @x = options[:x]
       @y = options[:y]
-
-      @anim = Chingu::Animation.new( :loop => false, :file => "media/gfx/explosion.png", :size=>[50,50], :delay => 10)
-		@x_vel = rand(0,10)
-
+		
+		
+      @anim = Chingu::Animation.new( :loop => false, :file => "media/gfx/explosion_strip.png", :size=>[29,62], :delay => 1).retrofy
       @image = @anim.next
 		
       self.factor = $window.object_factor
