d985afff2ce6b7d63135868d0714f2b45520f1a9
Bryan Glazer <bglazer@uark.edu>
2012-03-02 22:29:24 -0600

    Added frags to asteroid. Give frags to explosion when it's created.

--- lib/planet_defense/explosion.rb
+++ lib/planet_defense/explosion.rb
@@ -1,16 +1,19 @@
 module PlanetDefense
   class Explosion < Chingu::GameObject
     
-    def initialize( options )
+    def initialize( options, frags )
       super
       @x = options[:x]
       @y = options[:y]
 
       @anim = Chingu::Animation.new( :loop => false, :file => "media/gfx/explosion.png", :size=>[50,50], :delay => 10)
+		@x_vel = rand(0,10)
+
       @image = @anim.next
+		
       self.factor = $window.object_factor
     end
-  
+    
     def update    
       @image = @anim.next
     
@@ -19,6 +22,7 @@ module PlanetDefense
     
 	 def draw
 		@image.draw(@x,@y, 1)
+		#3.times( @frags.draw(@x, @y
 	 end
 	 
 	 def x
