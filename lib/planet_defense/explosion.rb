19271098b9d84c3486f354fad5c87ec372e5f947
Bryan Glazer <bglazer@uark.edu>
2012-03-04 16:58:24 -0600

    Got asteroids to break apart the right way

--- lib/planet_defense/explosion.rb
+++ lib/planet_defense/explosion.rb
@@ -1,38 +1,55 @@
 module PlanetDefense
-  class Explosion < Chingu::GameObject
+	class Explosion < Chingu::GameObject
     
     def initialize( options, frags )
       super
-      @x = options[:x]
-      @y = options[:y]
+		@life = 20
+		@steps = 0
+      @@init_x = options[:x]
+      @@init_y = options[:y]
 		@frags = options[:frags]
       @anim = Chingu::Animation.new( :loop => false, :file => "media/gfx/explosion_strip.png", :size=>[29,62], :delay => 1).retrofy
-      @image = @anim.next
+      @expl = @anim.next
 		
 		@rot = @vel_angular = rand(2) + 1
-		@vel_x = Array.new(3) { |vel| vel = rand(rand(9) + 1 )
-		@vel_y = Array.new(3) { |vel| vel = rand(rand(9) + 1 )
+		@angle = @vel_angular
+		
+		@vel_x = Array.new(3) { |vel| vel = rand(rand(9) + 1 ) }
+		@vel_y = Array.new(3) { |vel| vel = rand(rand(9) + 1 ) }
+		@x = Array.new(3) { @@init_x }
+		@y = Array.new(3) { @@init_y }
 		
       self.factor = $window.object_factor
     end
     
     def update    
       @image = @anim.next
-    
-      destroy if outside_window?
+		
+		@angle += @vel_angular
+		3.times{ |i| @x[i] += @vel_x[i]}
+		3.times{ |i| @y[i] += @vel_y[i]}
+		
+		@steps++
+		
+      destroy if @steps >= @life
     end
     
 	 def draw
-		@image.draw(@x,@y, 1)
-		@frags.each{ |frag| frag.draw
+		@expl.draw(@@init_x,@@init_y, 1)
+		@i = 0
+		@frags.each do
+		|f| f.draw_rot(@x[@i], @y[@i], 1, @angle)
+		@i = @i + 1
+		end
 	 end
 	 
 	 def x
       @x  
-    end
+	 end
   
-    def y
+	 def y
       @y
-    end
-  end
+	 end
+	 
+	end
 end
