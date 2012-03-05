7785fbd7b673e441fd7d2bb02415e98b64a51637
Addam Hardy <addam.hardy@gmail.com>
2012-03-05 13:46:35 -0600

    adding in other image

--- lib/planet_defense/explosion.rb
+++ lib/planet_defense/explosion.rb
@@ -2,24 +2,24 @@ module PlanetDefense
 	class Explosion < Chingu::GameObject
     
     def initialize( options )
-      super
-		@life = 20
-		@steps = 0
-      @@init_x = options[:x]
-      @@init_y = options[:y]
-		@frags = options[:frags]
-      @anim = Chingu::Animation.new( :loop => false, :file => "media/gfx/explosion_strip.png", :size=>[29,62], :delay => 1).retrofy
-      @expl = @anim.next
-		
-		@rot = @vel_angular = rand(2) + 1
-		@angle = @vel_angular
-		
-		@vel_x = Array.new(3) { |vel| vel = rand(rand(9) + 1 ) }
-		@vel_y = Array.new(3) { |vel| vel = rand(rand(9) + 1 ) }
-		@x = Array.new(3) { @@init_x }
-		@y = Array.new(3) { @@init_y }
-		
-      self.factor = $window.object_factor
+			super
+			@life = 20
+			@steps = 0
+			@@init_x = options[:x]
+			@@init_y = options[:y]
+			@frags = options[:frags]
+			@anim = Chingu::Animation.new( :loop => false, :file => "media/gfx/explosion.png", :size=>[50,50], :delay => 10)
+			@expl = @anim.next
+
+			@rot = @vel_angular = rand(2) + 1
+			@angle = @vel_angular
+
+			@vel_x = Array.new(3) { |vel| vel = rand(rand(9) + 1 ) }
+			@vel_y = Array.new(3) { |vel| vel = rand(rand(9) + 1 ) }
+			@x = Array.new(3) { @@init_x }
+			@y = Array.new(3) { @@init_y }
+
+			self.factor = $window.object_factor
     end
     
     def update    
