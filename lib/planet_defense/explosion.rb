e81d83229ad249b1998558656130704cc7b3ea1d
Addam Hardy <addam.hardy@gmail.com>
2012-03-05 14:03:41 -0600

    Updated explosion

--- lib/planet_defense/explosion.rb
+++ lib/planet_defense/explosion.rb
@@ -8,7 +8,7 @@ module PlanetDefense
 			@@init_x = options[:x]
 			@@init_y = options[:y]
 			@frags = options[:frags]
-			@anim = Chingu::Animation.new( :loop => false, :file => "media/gfx/explosion.png", :size=>[50,50], :delay => 10)
+			@anim = Chingu::Animation.new( :loop => false, :file => "media/gfx/explosion.png", :size=>[50,50], :delay => 15)
 			@expl = @anim.next
 
 			@rot = @vel_angular = rand(2) + 1
@@ -23,33 +23,33 @@ module PlanetDefense
     end
     
     def update    
-      @expl = @anim.next
-		
-		@angle += @vel_angular
-		3.times{ |i| @x[i] += @vel_x[i]}
-		3.times{ |i| @y[i] += @vel_y[i]}
-		
-		@steps = @steps+1
-		
-      destroy if @steps >= @life
+			@expl = @anim.next
+
+			@angle += @vel_angular
+			3.times{ |i| @x[i] += @vel_x[i]}
+			3.times{ |i| @y[i] += @vel_y[i]}
+
+			@steps = @steps+1
+
+			destroy if @steps >= @life
     end
     
-	 def draw
-		@expl.draw(@@init_x,@@init_y, 1)
-		@i = 0
-		@frags.each do
-		|f| f.draw_rot(@x[@i], @y[@i], 1, @angle)
-		@i = @i + 1
+		def draw
+			@expl.draw(@@init_x,@@init_y, 5)
+			@i = 0
+			@frags.each do |f| 
+				f.draw_rot(@x[@i], @y[@i], 1, @angle)
+				@i = @i + 1
+			end
 		end
-	 end
-	 
-	 def x
-      @x  
-	 end
-  
-	 def y
-      @y
-	 end
-	 
+
+		def x
+		  @x  
+		end
+
+		def y
+		  @y
+		end
+
 	end
 end
