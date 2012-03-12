6f45867150798197569d6dd3e15773a6345607d6
Bryan Glazer <bglazer@uark.edu>
2012-03-12 18:34:43 -0500

    Got text input class to start working.

--- lib/planet_defense/text_input.rb
+++ lib/planet_defense/text_input.rb
@@ -1,7 +1,15 @@
 module PlanetDefense
 	class TextInput < Chingu::GameObject
 		def initialize(options)
+			@x = options[:x]
+			@y = options[:y]
 			
+			$window.text_input = Gosu::TextInput.new()
+			@text  = Chingu::Text.create(:x => @x, :y => @y)
+		end
+		
+		def update
+			@text.text = $window.text_input.text
 		end
 	end
 end
