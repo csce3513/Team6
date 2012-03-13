f8094a13d77b8c674212150765468a148b7e5d87
Bryan Glazer <bglazer@uark.edu>
2012-03-12 22:15:32 -0500

    Got the text input working in save game state

--- lib/planet_defense/save_game.rb
+++ lib/planet_defense/save_game.rb
@@ -2,18 +2,55 @@ require 'yaml'
 
 module PlanetDefense
 	class SaveGame < Chingu::GameState
+	
 		def initialize( options = {})
 			super
+			@font = Gosu::Font.new($window, "media/fonts/MuseoSans_300.otf", 24)
+			@text_input = PlanetDefense::TextInput.create(:x => $window.width/2-100, :y => $window.height/2, 
+														:width => 200, :height => @font.height*2, :font => @font)			
+			@background_image = Gosu::Image.new($window, "media/gfx/space.jpg", true)
+			@title_image = Gosu::Image.new($window, "media/gfx/title.png", true)
+
+			self.input = { 
+				:up => :move_up,
+				:down => :move_down,
+				:left => :move_left,
+				:right => :move_right,
+			}
+		end
+		
+		
+		def setup
+		  @game_objects.destroy_all
+
+		end
+		
+		  
+		def move_up
+		end
+		
+		def move_down		  
+		end
+		
+		def move_left
+		end
+
+		def move_right
 		end
 		
 		def update
+			super
+			@text_input.update
 		end
 		
 		def draw
+			super
+			@background_image.draw(0,0,0)
+			@title_image.draw(($window.width/2)-@title_image.width/2,100,50)
 		end
 		
 		def self.save
-			YAML::dump($window.game_objects)
+			
 		end
 	end
 end
--- lib/planet_defense/text_input.rb
+++ lib/planet_defense/text_input.rb
@@ -1,15 +1,28 @@
 module PlanetDefense
 	class TextInput < Chingu::GameObject
 		def initialize(options)
+			super
 			@x = options[:x]
 			@y = options[:y]
+			@width = options[:width]
+			@height = options[:height]
+			@font = options[:font]
 			
 			$window.text_input = Gosu::TextInput.new()
 			@text  = Chingu::Text.create(:x => @x, :y => @y)
 		end
 		
 		def update
+			super
 			@text.text = $window.text_input.text
+		end
+		
+		def draw
+			super
+			@rect = Chingu::Rect.new(@x - @width/10, @y - @height/4, @width, @height)
+			@color = Gosu::Color.argb(0xff808080)
+			$window.fill_rect(@rect, @color, 55)
+			@font.draw(@text.text, @x, @y, 55, 1)
 		end
 	end
 end
